efb9abe1f848229c6b889a3e3f83272f61ad4d6f
Royhdk <76113160+Royhdk@users.noreply.github.com>
2020-12-16 22:39:45 +0800

    Add files via upload

---
+++ 太极 .py
@@ -0,0 +1,28 @@
+import turtle
+t = turtle
+t.pensize(5)
+t.speed(5)
+t.pencolor ("black")
+t.penup()
+t.right(90)
+t.forward(200)
+t.left(90)
+t.pendown()
+t.begin_fill()
+t.circle(200,180)
+t.end_fill()
+t.circle(200,180)
+t.circle(100,180)
+t.right(180)
+t.circle(100,-180)
+t.left(90)
+t.forward(100)
+t.dot(200,"black")
+t.dot(100,"white")
+t.penup()
+t.forward(200)
+t.pendown()
+t.dot(200,"white")
+t.dot(100,"black")
+
+
